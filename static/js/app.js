37b28580f15d5dee19766e8b55b6f6f3579e2443
wrube <bjminwruck@gmail.com>
2024-03-12 19:22:45 +1030

    modified code

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ Functions used in this script below
  * @param {string} activeID     onchange sample ID from dropdown
  */
 function optionChanged(activeID) {
-    
+
      // use the global dataPromise to access data
     dataPromise.then(jsonData => {  
         generatePlots(activeID, jsonData);
@@ -219,60 +219,6 @@ function metaDataTable(dataObj) {
     });
 }
 
-
-/**
- * Use Plotly to post a Gauge meter for scrubs per week for a particular test subject ID to HTML.
- * 
- * 
- * @param {Object} dataObj Metadata object for a particular Test Subject ID
- */
-function gaugePlot(dataObj) {
-
-    // set variables
-    const washFreq = dataObj.wfreq;
-    const range = [0, 9];
-
-    // set Plotly input
-    const trace = {
-        type: 'indicator',
-        mode: 'gauge+number',
-        title: {
-            text: "scrubs per week",
-            font: {
-                size: 16
-            }
-        },
-        value: washFreq,
-        gauge: {
-          axis: { range: range,
-                nticks: 10},
-          bar: { color: 'green' },
-          bgcolor: 'white',
-          borderwidth: 2,
-          bordercolor: 'gray',
-          shape: "angular",
-          steps: [
-            { range: range, color: 'lightgray' }
-          ],
-          threshold: {
-            line: { color: 'black', width: 4 },
-            thickness: 0.75,
-            value: washFreq
-          }
-        }
-      }
-
-    const data = [trace];
-
-    const layout = {
-        title: "Belly Button Washing Frequency",
-        font: {
-            size: 18
-        }
-    }
-
-    Plotly.newPlot('gauge', data, layout);
-}
 
 
 /**
